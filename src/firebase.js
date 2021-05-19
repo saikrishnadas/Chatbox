db6a411045f371278081dc9914fb52ead5dc0914
Sai <saikrishnadas666@gmail.com>
2021-05-19 22:29:21 +0530

    adding header,sidebar and firebase config

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  //Your config
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
